21469f6846a1b45eae22394749bda646896d7fab
MaysharaF <mayshara@alunos.utfpr.edu.br>
2022-04-10 11:36:45 -0300

    Refatorando inteface MenuEntryClickListener

--- src/main/java/br/edu/utfpr/dv/sireata/component/MenuEntry.java
+++ src/main/java/br/edu/utfpr/dv/sireata/component/MenuEntry.java
@@ -10,65 +10,71 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.Window;
 
 public class MenuEntry extends HorizontalLayout {
-	
-	public MenuEntry(String caption, int level, String navigateTo){
+
+	public MenuEntry(String caption, int level, String navigateTo) {
 		Link label = new Link(caption, null);
-		
-		this.addLayoutClickListener(new LayoutClickListener() {            
+
+		this.addLayoutClickListener(new LayoutClickListener() {
 			@Override
-	        	public void layoutClick(LayoutClickEvent event) {
-					UI.getCurrent().getNavigator().navigateTo(navigateTo);
-	            }
+			public void layoutClick(LayoutClickEvent event) {
+				UI.getCurrent().getNavigator().navigateTo(navigateTo);
+			}
 		});
-		
+
 		this.addLabel(label, level);
 	}
-	
-	public MenuEntry(String caption, int level){
+
+	public MenuEntry(String caption, int level) {
 		Label label = new Label(caption);
-		
+
 		this.addLabel(label, level);
 	}
-	
-	public MenuEntry(String caption, int level, Window window){
+
+	public MenuEntry(String caption, int level, Window window) {
 		Link label = new Link(caption, null);
-		
-		this.addLayoutClickListener(new LayoutClickListener() {            
+
+		this.addLayoutClickListener(new LayoutClickListener() {
 			@Override
-	        	public void layoutClick(LayoutClickEvent event) {
-					UI.getCurrent().addWindow(window);
-	            }
+			public void layoutClick(LayoutClickEvent event) {
+				UI.getCurrent().addWindow(window);
+			}
 		});
-		
+
 		this.addLabel(label, level);
 	}
-	
-	public MenuEntry(String caption, int level, MenuEntryClickListener clickListener){
+
+	public interface MenuEntryClickListener {
+
+		public void menuClick();
+
+	}
+
+	public MenuEntry(String caption, int level, MenuEntryClickListener clickListener) {
 		Link label = new Link(caption, null);
-		
-		this.addLayoutClickListener(new LayoutClickListener() {            
+
+		this.addLayoutClickListener(new LayoutClickListener() {
 			@Override
-	        	public void layoutClick(LayoutClickEvent event) {
-					clickListener.menuClick();
-	            }
+			public void layoutClick(LayoutClickEvent event) {
+				clickListener.menuClick();
+			}
 		});
-		
+
 		this.addLabel(label, level);
 	}
-	
-	private Label getIdent(int level){
+
+	private Label getIdent(int level) {
 		Label label = new Label("");
-		
+
 		label.setWidth(String.valueOf(level * 10) + "px");
-		
+
 		return label;
 	}
-	
-	private void addLabel(Component label, int level){
-		if(level > 0){
+
+	private void addLabel(Component label, int level) {
+		if (level > 0) {
 			this.addComponent(this.getIdent(level));
 		}
-		
+
 		this.addComponent(label);
 	}
 
--- src/main/java/br/edu/utfpr/dv/sireata/component/MenuEntryClickListener.java
+++
@@ -1,7 +0,0 @@
-package br.edu.utfpr.dv.sireata.component;
-
-public interface MenuEntryClickListener {
-	
-	public void menuClick();
-
-}
